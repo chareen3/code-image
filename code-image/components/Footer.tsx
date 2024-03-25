38a58b595f391633ca31b871080504c767bf4992
Charith Weerasinghe <33430068+chareen3@users.noreply.github.com>
2024-03-26 01:41:27 +0530

    Update Footer.tsx

--- code-image/components/Footer.tsx
+++ code-image/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className="flex items-center gap-10 py-16">
       <Link
-        href="https://thecodedealer.com"
+        href="#"
         className="flex items-center text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500"
       >
         <span className="pr-2"></span>
